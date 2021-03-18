884cfda85c42951ffa978efcbc10fb63c4f4a0d2
John Rowley <outsidethebox@eircom.net>
2021-03-18 07:46:34 -0700

    various

--- flashcards.py
+++ flashcards.py
@@ -1,13 +1,17 @@
 from flask import (Flask, render_template)
 from datetime import datetime
+from model import db
 app = Flask(__name__)
 
 
 @app.route("/welcome")
 def welcomex():
-    return render_template("demo.html",message='This is a message')
+    return render_template("demo.html",message='This is a message',age=2)
 
-
+@app.route("/card")
+def card():
+    card=db[0]
+    return render_template("cardx.html",card=card)
 
 @app.route("/")
 def welcome():
