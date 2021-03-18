a1a52d8690433a26c4fb38c2620abf0585ae57ed
John Rowley <outsidethebox@eircom.net>
2021-03-18 07:35:12 -0700

    varoius

--- flashcards.py
+++ flashcards.py
@@ -1,17 +1,29 @@
-from flask import Flask
+from flask import (Flask, render_template)
 from datetime import datetime
 app = Flask(__name__)
 
+
+@app.route("/welcome")
+def welcomex():
+    return render_template("demo.html",message='This is a message')
+
+
+
 @app.route("/")
-def hello():
-    return render_template(
-        "welcome.html"
-        
-    )
+def welcome():
+     return "Hello World"
 
 @app.route("/date")
 def date():
    return "This page was served at " + str(datetime.now())
+
+counter = 0
+#lesson 2
+@app.route("/count_views")
+def count_views():
+  global counter
+  counter = counter+1
+  return "This page was served at " + str(counter) + " times"
 
 if __name__ == "__main__":
   app.run()
---
+++ readme.txt
@@ -0,0 +1,8 @@
+python -m venv myproject
+
+source myproject/bin/activate
+
+myproject\Scripts\activate
+
+FLASK_APP=flashcards.py
+FLASK_ENV=development
